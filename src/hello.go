bd1976f66cb49414cfc5607189a61a245846e3f4
Adrian Ko <adrianchko@gmail.com>
2015-06-15 09:50:52 +0100

    Code linting

--- src/hello.go
+++ src/hello.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"time"
-	"math"
 	"container/list"
+	"fmt"
+	"math"
 	"math/rand"
+	"time"
 )
 
 func add(x int, y int) int {
@@ -18,7 +18,7 @@ func swap(x, y string) (string, string) {
 
 func split(sum int) (x, y int) {
 	x = sum * 4 / 9
-	y = sum - x 
+	y = sum - x
 	return
 }
 
@@ -26,16 +26,16 @@ func loops() {
 	for i := 0; i < 5; i++ {
 		fmt.Println(i)
 	}
-	
+
 	j := 1
-	
+
 	fmt.Println("while")
-	
+
 	for j < 5 {
 		j += j
 		fmt.Println(j)
 	}
-	
+
 	fmt.Println(j)
 }
 
@@ -51,58 +51,58 @@ func listEx() {
 	list.PushBack(4)
 	list.PushBack(1)
 	list.PushFront(7)
-	
+
 	for e := list.Front(); e != nil; e = e.Next() {
 		fmt.Println(e.Value)
 	}
 }
 
-func randInt(max int) (int) {
+func randInt(max int) int {
 	rand.Seed(time.Now().UTC().UnixNano())
-	
+
 	return rand.Intn(max)
 }
 
-func ifScopedVars() (float64) {
-	if v:= math.Pow(5, 2); v < 20 {
+func ifScopedVars() float64 {
+	if v := math.Pow(5, 2); v < 20 {
 		return v
 	}
-	
+
 	return 20
 }
 
 func deferred() {
 	defer fmt.Println("world")
-	
+
 	fmt.Println("hello")
 }
 
 func deferredStack() {
 	fmt.Println("\n")
 	fmt.Println("counting")
-	
+
 	for i := 0; i < 5; i++ {
 		defer fmt.Println(i)
 	}
-	
+
 	fmt.Println("done")
 }
 
 func slices() {
 	s := []int{2, 3, 5, 7, 11, 13}
 	fmt.Println("s ==", s)
-	
+
 	for i := 0; i < len(s); i++ {
 		fmt.Printf("s[%d] == %d\n", i, s[i])
 	}
-	
+
 	//create slice
 	fmt.Println(s[1:4])
-	
+
 	//make slice
 	a := make([]int, 0, 5)
 	fmt.Println(a)
-	
+
 	//range
 	for _, v := range s {
 		fmt.Printf("%d\n", v)
@@ -116,51 +116,51 @@ type Vertex struct {
 func main() {
 	fmt.Println("Hello, world")
 	/*
-	Go basic types
-	bool
-	string
-	int int8 int16 int32 int64
-	uint uint8 uint16 uint32 uint64 uintpr
-	byte (alias for uint8)
-	rune (alias for int32)
-	float32 float64
-	complex64 complex128
+		Go basic types
+		bool
+		string
+		int int8 int16 int32 int64
+		uint uint8 uint16 uint32 uint64 uintpr
+		byte (alias for uint8)
+		rune (alias for int32)
+		float32 float64
+		complex64 complex128
 	*/
-	i := 3 //implicit type
+	i := 3        //implicit type
 	var m int = 2 //declared type
 	var n int = int(4.0)
 	const Hello = "World"
 	loops()
-	
+
 	fmt.Println(time.Now())
-	
+
 	arrayEx()
 	listEx()
-	
+
 	fmt.Println("Random numbers: ")
-	
+
 	for i := 0; i < 5; i++ {
 		fmt.Println(randInt(100))
 	}
-	
+
 	fmt.Println(add(i, m))
 	hello := "hello"
 	world := "world"
 	hello, world = swap(hello, world)
 	fmt.Println("hello: " + hello)
 	fmt.Println("world: " + world)
-	
+
 	fmt.Println(split(n))
 	fmt.Println(ifScopedVars())
 	deferred()
 	deferredStack()
-	
-	t := 2 //var
+
+	t := 2  //var
 	p := &t //pointer
 	*p = 21
-	
-	fmt.Println(p) //mem address
-	fmt.Println(*p) //mem address value
+
+	fmt.Println(p)    //mem address
+	fmt.Println(*p)   //mem address value
 	v := Vertex{1, 2} //use Struct (collection of fields)
 	fmt.Println(v.X)
 	slices()
